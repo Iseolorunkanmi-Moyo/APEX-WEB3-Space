e7cb4232c09e90c7d0e8bab323f8ccca03695bb0
Oluwatomisin <kanmitomi@gmail.com>
2024-06-04 16:10:59 +0100

    third commit

--- index.js
+++ index.js
@@ -9,17 +9,88 @@ var menucount = 0;
 
 function menuFunction(){
     if (menucount == 0) {
-        navSecondDiv.style.display = "flex"
-        mainSection.style.filter = " blur(5px)"
-        menucount = 1
+        openMenu()
     }
 
     else if (menucount == 1) {
-        navSecondDiv.style.display = "none"
-        mainSection.style.filter = " blur(0px)"
-        menucount = 0
+        closeMenu()
     }
-
 }
 
+function openMenu(){
+    navSecondDiv.style.display = "flex"
+    mainSection.style.filter = " blur(5px)"
+    menucount = 1
+}
 
+function closeMenu(){
+    navSecondDiv.style.display = "none"
+    mainSection.style.filter = " blur(0px)"
+    menucount = 0
+}
+
+var homeLink = document.getElementById("homeLink")
+var homeSection  = document.getElementById("home")
+
+homeLink.addEventListener("click", () => {
+    homeSection.style.display = "flex"
+    tokenomicsSection.style.display = "none"
+    earnPointsSection.style.display = "none"
+    contactSection.style.display = "none"
+
+
+    var WIDTH_LIMIT = 1044;
+    var windowWidth = window.innerWidth;
+   
+    if(windowWidth <= WIDTH_LIMIT) {
+        closeMenu()
+    } 
+})
+
+var tokenomicsLink = document.getElementById("tokenomicsLink")
+var tokenomicsSection  = document.getElementById("tokenomics")
+
+tokenomicsLink.addEventListener("click", () => {
+    homeSection.style.display = "none"
+    tokenomicsSection.style.display = "flex"
+    earnPointsSection.style.display = "none"
+    contactSection.style.display = "none"
+    var WIDTH_LIMIT = 1044;
+    var windowWidth = window.innerWidth;
+   
+    if(windowWidth <= WIDTH_LIMIT) {
+        closeMenu()
+    } 
+})
+
+var earnPointsLink = document.getElementById("earnPointsLink")
+var earnPointsSection  = document.getElementById("earnPoints")
+
+earnPointsLink.addEventListener("click", () => {
+    homeSection.style.display = "none"
+    tokenomicsSection.style.display = "none"
+    earnPointsSection.style.display = "flex"
+    contactSection.style.display = "none"
+    var WIDTH_LIMIT = 1044;
+    var windowWidth = window.innerWidth;
+   
+    if(windowWidth <= WIDTH_LIMIT) {
+        closeMenu()
+    } 
+})
+
+var contactLink = document.getElementById("contactLink")
+var contactSection  = document.getElementById("contact")
+
+contactLink.addEventListener("click", () => {
+    homeSection.style.display = "none"
+    tokenomicsSection.style.display = "none"
+    earnPointsSection.style.display = "none"
+    contactSection.style.display = "flex"
+    var WIDTH_LIMIT = 1044;
+    var windowWidth = window.innerWidth;
+   
+    if(windowWidth <= WIDTH_LIMIT) {
+        closeMenu()
+    } 
+})
